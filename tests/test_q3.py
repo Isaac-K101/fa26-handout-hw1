4534b34c5bcd1b70c4e62441a95ac64851f6896f
Isaac Kim <isaackim1105@gmail.com>
2026-09-16 23:57:11 -0400

    space

--- tests/test_q3.py
+++ tests/test_q3.py
@@ -6,7 +6,7 @@ sys.path.append('.')
 from src.q3 import capitalize_words
 
 
-"""Please write your tests here."""
+"""Please write your tests here.""" 
 
 # test basic functionality
 def test_basic():
